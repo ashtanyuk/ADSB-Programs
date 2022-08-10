9087ab4abb50254d8fd1ae56c2c5adf7d5a373c0
Anton Shtanyuk <ashtanyuk@gmail.com>
2022-08-10 15:09:56 +0300

    New directory structure

---
+++ part03/part03.h
@@ -0,0 +1,6 @@
+#ifndef _PART03_H_
+#define _PART03_H_
+
+int strstr(char* s, char* p);
+
+#endif // _PART03_H_
---
+++ test/tests.cpp
@@ -0,0 +1,9 @@
+// Copyright 2022 A.SHT
+#include <gtest/gtest.h>
+#include <string>
+#include "part03.h"
+
+TEST(part03, search1) {
+	
+	ASSERT_TRUE(true);
+}
