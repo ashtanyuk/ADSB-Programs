f522be861722cd67b5c3ddff47ccd6e25d079e7a
Anton Shtanyuk <ashtanyuk@gmail.com>
2022-08-10 15:49:22 +0300

    strSearch fixed

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,5 +6,5 @@
 TEST(part03, search1) {
 	const char * source = "00000000111111000000000000";
 	const char * str = "111";
-	ASSERT_TRUE(strSearch(source) > 0);
+	ASSERT_TRUE(strSearch(source, str) > 0);
 }
