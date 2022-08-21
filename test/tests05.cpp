f7a79d35a0a474fd28ec43969cdaee9bff6cae5e
Anton Shtanyuk <ashtanyuk@gmail.com>
2022-08-21 20:21:01 +0300

    tests added for part05

--- test/tests05.cpp
+++ test/tests05.cpp
@@ -10,10 +10,61 @@ TEST(part05, unratio1) {
    ASSERT_TRUE(arr[0] == 1 && arr[1] == 5 && arr[2] == 8 );
 }
 
+TEST(part05, unratio2) {
+   int arr[]  = {5, 8, 1, 4, 9, 0, 6, 12, 2, 4};
+   int arrS[] = {0, 1, 2, 4, 4, 5, 6, 8, 9, 12};
+
+   unratioSort(arr, 10);
+  
+   for (int i = 0; i < 10; ++i) 
+      EXPECT_EQ(arr[i], arrS[i]) << "Arrays differ at index " << i;
+}
+
+
 TEST(part05, insertion1) {
    int arr[] = {5, 8, 1};
    insertionSort(arr, 3);
    ASSERT_TRUE(arr[0] == 1 && arr[1] == 5 && arr[2] == 8 );
+}
+
+TEST(part05,  insertion2) {
+   int arr[]  = {5, 8, 1, 4, 9, 0, 6, 12, 2, 4};
+   int arrS[] = {0, 1, 2, 4, 4, 5, 6, 8, 9, 12};
+
+   insertionSort(arr, 10);
+  
+   for (int i = 0; i < 10; ++i) 
+      EXPECT_EQ(arr[i], arrS[i]) << "Arrays differ at index " << i;
+}
+
+TEST(part05,  selection) {
+   int arr[]  = {5, 8, 1, 4, 9, 0, 6, 12, 2, 4};
+   int arrS[] = {0, 1, 2, 4, 4, 5, 6, 8, 9, 12};
+
+   selectionSort(arr, 10);
+  
+   for (int i = 0; i < 10; ++i) 
+      EXPECT_EQ(arr[i], arrS[i]) << "Arrays differ at index " << i;
+}
+
+TEST(part05,  bubble1) {
+   int arr[]  = {5, 8, 1, 4, 9, 0, 6, 12, 2, 4};
+   int arrS[] = {0, 1, 2, 4, 4, 5, 6, 8, 9, 12};
+
+   bubbleSort1(arr, 10);
+  
+   for (int i = 0; i < 10; ++i) 
+      EXPECT_EQ(arr[i], arrS[i]) << "Arrays differ at index " << i;
+}
+
+TEST(part05,  bubble2) {
+   int arr[]  = {5, 8, 1, 4, 9, 0, 6, 12, 2, 4};
+   int arrS[] = {0, 1, 2, 4, 4, 5, 6, 8, 9, 12};
+
+   bubbleSort2(arr, 10);
+  
+   for (int i = 0; i < 10; ++i) 
+      EXPECT_EQ(arr[i], arrS[i]) << "Arrays differ at index " << i;
 }
 
 TEST(part05, quick1) {
