76b3cb21c77383ad7ebce5c33686abc3bc008cc6
Anton Shtanyuk <ashtanyuk@gmail.com>
2022-08-22 12:21:32 +0300

    tests added for part06

--- part06/sstack.h
+++ part06/sstack.h
@@ -1,27 +1,27 @@
 // Copyright 2022
 
 template<typename T, int size = 100>
-class TStack {
+class SStack {
    private:
      T arr[size];
      int top;
    public:
-     TStack():top(-1) { }
+     SStack():top(-1) { }
      T get() const { 
         return arr[top];
      }
      bool isEmpty() const {
-        return top==-1;
+        return top == -1;
      }
      bool isFull() const {
-        return top==size-1;
+        return top == size - 1;
      }
      void pop() {
-        if(top>=0)
+        if(top >= 0)
           top--;
      }
      void push(T item) {
-       if(top<size-1)
-         arr[++top]=item;
+       if(top < size - 1)
+         arr[++top] = item;
      }
 };
---
+++ test/tests06.cpp
@@ -0,0 +1,12 @@
+// Copyright 2022 A.SHT
+#include <gtest/gtest.h>
+#include <string>
+#include "sstack.h"
+#include "dstack.h"
+
+TEST(part06, sstack1) {
+
+   SStack stack; 
+
+   ASSERT_TRUE(stack.isEmpty() == true);
+}
