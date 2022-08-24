375d19d4362ed97000683821dfc2b4a761e5cbb7
Anton Shtanyuk <ashtanyuk@gmail.com>
2022-08-24 12:09:39 +0300

    tests added for part08

--- test/tests08.cpp
+++ test/tests08.cpp
@@ -14,12 +14,6 @@ TEST(part08, slist1) {
 TEST(part08, slist2) {
 
     SList<int> list; 
-			 list.push(1);
-			 list.push(2);
-			 list.push(3);
-			 list.push(4);
-			 list.push(5);
-
     ASSERT_TRUE(true);
 }
 
