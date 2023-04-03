5b939f569663a50793c5aea842c913e52d7092c1
Anton Shtanyuk <ashtanyuk@gmail.com>
2023-04-03 22:46:03 +0300

    Added part08 tests

--- test/tests08.cpp
+++ test/tests08.cpp
@@ -37,7 +37,7 @@ TEST(part08, slist_throw_1) {
         FAIL() << "Expected std::string";
     }
     catch(std::string const & err) {
-        EXPECT_EQ(err,std::string("empty!"));
+        EXPECT_EQ(err,std::string("Empty!"));
     }
     catch(...) {
         FAIL() << "Expected std::string";
