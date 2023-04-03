de0c90d89612096a207cb745cc3c16017416496d
Anton Shtanyuk <ashtanyuk@gmail.com>
2023-04-03 22:02:22 +0300

    Added part08 tests

--- test/tests08.cpp
+++ test/tests08.cpp
@@ -4,19 +4,42 @@
 #include "slist.h"
 #include "dlist.h"
 
-TEST(part08, slist_print_1) {
+TEST(part08, slist_empty_1) {
 
    SList<int> list; 
-   int count = list.print();
-   ASSERT_EQ(count, 0);
+    int count = list.print();
+    ASSERT_EQ(count, 0);
 }
 
-TEST(part08, slist_print_2) {
+TEST(part08, slist_addTail_1) {
 
     SList<int> list; 
-				list.addTail(5);
-				list.addTail(6);
-				int count = list.print();
+    list.addTail(5);
+    list.addTail(6);
+    int count = list.print();
     ASSERT_EQ(count, 2);
 }
 
+TEST(part08, slist_empty_2) {
+
+    SList<int> list; 
+    list.addTail(5);
+    list.rmTail();
+    int count = list.print();
+    ASSERT_EQ(count, 0);
+}
+
+TEST(part08, slist_throw_1) {
+	
+    SList<int> list;
+    try {
+        list.rmHead();
+        FAIL() << "Expected std::string";
+    }
+    catch(std::string const & err) {
+        EXPECT_STREQ(err,std::string("empty!"));
+    }
+    catch(...) {
+        FAIL() << "Expected std::string";
+    }
+}
